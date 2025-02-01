d14a10741134cf439c5c8f7c5f56ac1d45b3f37c
Anderson.Santos <mister.x.angel@gmail.com>
2025-02-01 18:20:19 -0300

    Feat: adiciona o tratamento de exceções

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,5 +1,7 @@
-import {Request, Response} from "express";
+import {NextFunction, Request, Response} from "express";
 import {getFirestore} from "firebase-admin/firestore";
+import { ValidationError } from "../errors/validation.error";
+import { NotFoundError } from "../errors/not-found.error";
 
 type User = {
     id: number;
@@ -8,53 +10,63 @@ type User = {
 };
 
 export class UsersController {
-    static async getAll(req: Request, res: Response) {
+    static async getAll(req: Request, res: Response, next: NextFunction) {
         const snapshot = await getFirestore().collection("users").get();
         const users = snapshot.docs.map(doc => {
-            return {
-                id: doc.id,
-                ...doc.data()
-            };
+        return {
+            id: doc.id,
+            ...doc.data()
+        };
         });
         res.send(users);
     }
 
-    static async getById(req: Request, res: Response) {
+    static async getById(req: Request, res: Response, next: NextFunction) {
         let userId = req.params.id;
         const doc = await getFirestore().collection("users").doc(userId).get();
-        let user = {
-            id: doc.id,
-            ...doc.data()
-        };
-        res.send(user);
+        if (doc.exists) {
+            res.send({
+                id: doc.id,
+                ...doc.data()
+            });
+        } else {
+            throw new NotFoundError("Usuário não encontrado")
+        }
     }
 
-    static async save(req: Request, res: Response) {
+    static async save(req: Request, res: Response, next: NextFunction) {
         let user = req.body;
+        if (!user.email || user.email.length === 0) {
+            throw new ValidationError("E-mail obrigatório");
+        }
         const userSave = await getFirestore().collection("users").add(user);
         res.send({
             message: `Usuário ${userSave.id} criado com sucesso!`
         });
     }
 
-    static update(req: Request, res: Response) {
+    static async update(req: Request, res: Response, next: NextFunction) {
         let userId = req.params.id;
         let user = req.body as User;
-        getFirestore().collection("users").doc(userId).set({
-            nome: user.nome,
-            email: user.email
-        });
-        res.send({
-            message: "Usuário atualizado com sucesso!"
-        });
+        let docRef = getFirestore().collection("users").doc(userId);
+
+        if ((await docRef.get()).exists) {
+            await docRef.set({
+                nome: user.nome,
+                email: user.email
+            });
+            res.send({
+                message: "Usuário atualizado com sucesso!"
+            });
+        } else {
+            throw new NotFoundError("Usuário não encontrado");
+        }  
     }
     
-    static async delete(req: Request, res: Response) {
+    static async delete(req: Request, res: Response, next: NextFunction) {
         let userId = req.params.id;
         await getFirestore().collection("users").doc(userId).delete();
-        res.send({
-            message: "Usuário deletado com sucesso!"
-        });
+        res.status(204).end();
     }
 }
 
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { UsersController } from "../controllers/users.controller";
+import AsyncHandler from "express-async-handler";
 
 export const userRoutes = express.Router();
 
-userRoutes.get("/users", UsersController.getAll);
-userRoutes.get("/users/:id", UsersController.getById);
-userRoutes.post("/users", UsersController.save);
-userRoutes.put("/users/:id", UsersController.update);
-userRoutes.delete("/users/:id", UsersController.delete);
+userRoutes.get("/users", AsyncHandler(UsersController.getAll));
+userRoutes.get("/users/:id", AsyncHandler(UsersController.getById));
+userRoutes.post("/users", AsyncHandler(UsersController.save));
+userRoutes.put("/users/:id", AsyncHandler(UsersController.update));
+userRoutes.delete("/users/:id", AsyncHandler(UsersController.delete));
